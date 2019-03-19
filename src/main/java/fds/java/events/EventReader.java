6d331a4013a57cc481bf0d46a9c61ecb750d7b3c
NigeWarren <Nige@zink-digital.com>
2019-03-19 13:27:42 +0000

    Pretty print report

--- src/main/java/fds/java/events/EventReader.java
+++ src/main/java/fds/java/events/EventReader.java
@@ -6,6 +6,7 @@ import io.nats.streaming.SubscriptionOptions;
 
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.Stream;
 
 
 /**
@@ -13,7 +14,7 @@ import java.util.concurrent.CountDownLatch;
  */
 public class EventReader {
 
-    final static Map<String,Integer> document = new HashMap<>();
+
 
     final static Set<String> targets= new HashSet<String>(
                                 Arrays.asList(  "AdultSki",
@@ -21,42 +22,69 @@ public class EventReader {
                                                 "DanielBaudSkiGuide")
                                 );
 
-    final static void register(final String logLine) {
+    final static void register(final String logLine, final Map<String,Integer> document) {
         targets.forEach( ( target ) -> {
             if ( logLine.contains((target)) )
                 document.compute(target, (k, v) -> (v == null) ? 1 : v + 1);
         });
     }
 
-    final static void showDoc ( ) {
-        document.forEach ( (k,v) -> System.out.println(k + ":" + v));
-    }
 
     public static void main(String[] args) throws Exception {
 
-        // Connect to EventStore
-        final String clusterID = "test-cluster";
-        final String clientID = "event-reader";
-        final StreamingConnectionFactory cf = new StreamingConnectionFactory(clusterID, clientID);
-        final StreamingConnection sc = cf.createConnection();
+        final Map<String,Integer> document = new HashMap<>();
 
-        // Subscribe to the store for events
-        final String subject = "BBC7";
+        register("AdultSki", document);
+        register("AdultSki", document);
+        register("AdultSki", document);
 
-        // you may want to remove count down latch for the stream.
-        final CountDownLatch doneLatch = new CountDownLatch(1);
-        final SubscriptionOptions opts = new SubscriptionOptions.Builder().deliverAllAvailable().build();
-        sc.subscribe(subject, evt -> {
-            System.out.println(evt);
-            doneLatch.countDown();
-        } , opts);
+        register("WinterActivities", document);
+        register("WinterActivities", document);
+        register("WinterActivities", document);
 
-        doneLatch.await();
-        sc.close();
+        register("DanielBaudSkiGuide", document);
+
+        formatDoc(document);
+
+
+//        // Connect to EventStore
+//        final String clusterID = "test-cluster";
+//        final String clientID = "event-reader";
+//        final StreamingConnectionFactory cf = new StreamingConnectionFactory(clusterID, clientID);
+//        final StreamingConnection sc = cf.createConnection();
+//
+//        // Subscribe to the store for events
+//        final String subject = "BBC7";
+//
+//        // you may want to remove count down latch for the stream.
+//        final CountDownLatch doneLatch = new CountDownLatch(1);
+//        final SubscriptionOptions opts = new SubscriptionOptions.Builder().deliverAllAvailable().build();
+//        sc.subscribe(subject, evt -> {
+//            System.out.println(evt);
+//            doneLatch.countDown();
+//        } , opts);
+//
+//        doneLatch.await();
+//        sc.close();
 	}
 
 
+    final static void formatDoc(final Map<String,Integer> document ) {
 
+        cout("--------------------------------------------------------------------------------");
+        cout("| Giant Media - Usage Report                                                   |");
+        cout("--------------------------------------------------------------------------------");
+        document.forEach( (k,v) -> System.out.println(String.format("| %-49s|%26d |",k,v)) );
+        cout("--------------------------------------------------------------------------------");
+        final Stream<Integer> views = document.values().stream();
+        cout(String.format("| %-49s|%26d |","Total Views", views.reduce(0, Integer::sum)));
+        cout("--------------------------------------------------------------------------------");
+    }
+
+
+    final static void cout(final String str) {
+        System.out.println(str);
+    }
 
 
 }
